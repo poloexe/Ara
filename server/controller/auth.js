744956e5b358e03da00217e21cf4ad4598922c0e
poloexe <baruchefiok@gmail.com>
2026-05-28 10:49:29 +0100

    Added signup stuffs

---
+++ server/controller/auth.js
@@ -0,0 +1,37 @@
+import User from "../models/user.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, email, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ msg: "Passwords do not match" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ msg: "Email has already been registered" });
+    }
+
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+    });
+
+    if (user) {
+      return res.status(201).json({
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        msg: "Account created successfully!",
+      });
+    } else {
+      return res.status(400).json({ msg: "Invalid user data" });
+    }
+  } catch (error) {
+    console.error(`Signup error: ${error.message}`)
+      return res.status(500).json({ msg: "Something is wrong with the server" });
+    
+  }
+};
