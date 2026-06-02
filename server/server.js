465eca9f0b42bbeae0bda91f6e8b33118e63aa97
poloexe <baruchefiok@gmail.com>
2026-06-02 10:13:52 +0100

    Added the CORS config and updated server log message

--- server/server.js
+++ server/server.js
@@ -2,11 +2,19 @@ import express from "express";
 import "dotenv/config";
 import { connectDb } from "./config/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import productRoutes from "./routes/product.js";
 import cartRoutes from "./routes/cart.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
@@ -19,5 +27,5 @@ const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, async () => {
   await connectDb();
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
