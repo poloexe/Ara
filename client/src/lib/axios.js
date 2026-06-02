af41a0fd799132d10335e454840d977cbb0831f1
poloexe <baruchefiok@gmail.com>
2026-06-02 10:07:04 +0100

    Update axios instance to use base URL in the env file

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "/api"
+      : import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
